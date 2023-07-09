b0d3b4c3259f9998eca984cfb2dad0648ef8b060
Jay <jaykwon07@gmail.com>
2023-07-09 09:49:20 -0500

    8th commit

--- two_sum.py
+++ two_sum.py
@@ -25,16 +25,31 @@ target = 9
 
 # Implement below using nums and target:
 
-number_index = -1
-lst = []
-target = int(input("Target: "))
-nums = int(input("Number List: "))
-for i in range(0, nums):
-	ele = int(input())
+nums = [2, 7, 11, 15]
+target = 22
 
-	lst.append(ele)
-# print(lst)
-for number in range(len(lst)):
-  if lst[number] + lst[number] == target:
-    number_index == number
-    print(lst[number_index])
+number1 = -100
+number2 = -100
+
+# number 1 setting
+for number1 in nums:
+  #number1 = 2  number 2 setting
+  for number2 in nums:
+    if number1 == number2:
+      continue #break
+        #number2
+    if number1 + number2 == target:
+      # if number1 == number2:
+      #   continue #break
+      
+      print("Solution!")
+      print("number1: " + str(number1) + " // " + "number2: " + str(number2))
+      
+      # Getting indices of solution
+      index1 = nums.index(number1)
+      index2 = nums.index(number2)
+
+      print("index1: " + str(index1) + " // " + "index1: " + str(index2))
+      print("------------------------------------------------")
+# if number1 + number2 == target:
+#   print("Solution!")
